0e4e6ed9db58c18bf829c5fbbb0150a7c3ca0f0f
abako123 <babyabacus1@gmail.com>
2021-09-09 19:18:32 +0900

    11 drawer twst

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,47 @@
       color="primary"
       dark
     >
-      <v-app-bar-nav-icon/>
-      <v-toolbar-title>ABAKO-Simsan</v-toolbar-title>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title>ABAKO simsan</v-toolbar-title>
       <v-spacer/>
-      <v-btn icon to='/about'>
+      <v-btn icon to="/about">
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
-      <v-btn icon to='/'>
+      <v-btn icon to="/">
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
+      <v-divider></v-divider>
+    </v-navigation-drawer>
     <v-content>
       <router-view/>
     </v-content>
-
-    <v-footer app color="primary" dark fixes>
-      <v-spacer/>
+    <v-footer app color="primary" dark absolute>
+      <v-spacer></v-spacer>
       <div>&copy; {{ new Date().getFullYear() }}</div>
     </v-footer>
-
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
