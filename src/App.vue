65e6a8e5b7c5cf10965c66b9d7598e353b809eaf
abako123 <babyabacus1@gmail.com>
2021-09-09 18:54:46 +0900

    10 footer test

--- src/App.vue
+++ src/App.vue
@@ -5,51 +5,31 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-app-bar-nav-icon/>
+      <v-toolbar-title>ABAKO-Simsan</v-toolbar-title>
+      <v-spacer/>
+      <v-btn icon to='/about'>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon to='/'>
+        <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
 
-    <v-main>
+    <v-content>
       <router-view/>
-    </v-main>
+    </v-content>
+
+    <v-footer app color="primary" dark fixes>
+      <v-spacer/>
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  })
+  name: 'App'
 }
 </script>
