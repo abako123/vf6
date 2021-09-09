dd6dbfb6528fa374bd7dd824872b45fc0deaf44f
abako123 <babyabacus1@gmail.com>
2021-09-09 15:08:48 +0900

    5 thanks

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h2>Git Add</h2>
+    <h2>Git Thanks ^^</h2>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
